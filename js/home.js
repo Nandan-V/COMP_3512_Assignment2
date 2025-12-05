77039de941455b2562c720d236f81ab3da278710
Zain Choudhry <113311032+zain326@users.noreply.github.com>
2025-12-04 19:17:15 -0700

    added featured products

--- js/home.js
+++ js/home.js
@@ -4,5 +4,12 @@
 
 // renderHomeView: selects top sellers and displays them as cards on Home.
 function renderHome() {
-  
+    const container = document.getElementById('homeFeatured');
+    if (!container) return;
+    container.textContent = '';
+
+    for (let i = 0; i < state.products.length && i < 4; i++) {
+        const card = createProductCard(state.products[i]);
+        container.appendChild(card);
     }
+}
